9639f85bb2c4f1bf00b9721e83696ced21b86955
xiaohong <dawn2309@163.com>
2024-09-26 22:25:38 +0800

    feat: 添加租房列表页 付款方式搜索

--- entry/src/main/ets/models/RentRoomData/dataAny.ts
+++ entry/src/main/ets/models/RentRoomData/dataAny.ts
@@ -1,6 +1,13 @@
-export interface dataAny {
+export interface RentPriceDataAny {
   rentPriceList: any
   rentPrice: any
   isSearch: boolean
   searchRentPrice: any
+}
+
+export interface PaymentTypeDataAny  {
+  paymentTypeList: string[]
+  isSearch: boolean
+  paymentType: string
+  searchPaymentType: string
 }
